d806ef513789ea4925a4146c3f7da9cdbb621f45
williamsouef <williamsouef@gmail.com>
2019-12-06 15:46:44 +0100

    Initial Commit

---
+++ bmi calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bmi calculator
+//
+//  Created by William Souef on 06/12/2019.
+//  Copyright © 2019 William Souef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
